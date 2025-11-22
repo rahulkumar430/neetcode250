bda4ed7885d816a6f0597e18b96c4c7c33510819
Partha Pratim Sahu <parthasahu@192.168.1.7>
2025-11-23 00:09:01 +0530

    array and hashing problem 7th updated

--- Arrays & Hashing/7.remove_element.cpp
+++ Arrays & Hashing/7.remove_element.cpp
@@ -2,30 +2,45 @@
 using namespace std;
 
 int removeElement(vector<int>& nums, int val) {
-    int ans = 0;  // Count of elements not equal to val
+    int k = 0;  // Pointer to position to place next non-val element
     for (int i = 0; i < nums.size(); i++) {
         if (nums[i] != val) {
-            // Current element not val, increment count
-            ans++;
-        } else {
-            // Current element equals val, find next non-val element
-            int j = i + 1;
-            // Move j forward while elements are val to find a non-val to swap
-            while (j < nums.size() && nums[j] == val) {
-                j++;
-            }
-            if (j < nums.size()) {
-                // Swap the val at i with non-val at j
-                swap(nums[i], nums[j]);
-                ans++;  // Increment count as we swapped in a valid element at i
-            } else {
-                // No more non-val elements to swap, rest are val
-                break;
-            }
+            // If current element is not val, move it to index k
+            nums[k++] = nums[i];
         }
+        // If element is val, do nothing and skip it
     }
-    return ans;
+    // k now holds the count of elements not equal to val
+    // The first k elements of nums are the updated array without val
+    return k;
 }
+
+// Another approach
+// int removeElement(vector<int>& nums, int val) {
+//     int ans = 0;  // Count of elements not equal to val
+//     for (int i = 0; i < nums.size(); i++) {
+//         if (nums[i] != val) {
+//             // Current element not val, increment count
+//             ans++;
+//         } else {
+//             // Current element equals val, find next non-val element
+//             int j = i + 1;
+//             // Move j forward while elements are val to find a non-val to swap
+//             while (j < nums.size() && nums[j] == val) {
+//                 j++;
+//             }
+//             if (j < nums.size()) {
+//                 // Swap the val at i with non-val at j
+//                 swap(nums[i], nums[j]);
+//                 ans++;  // Increment count as we swapped in a valid element at i
+//             } else {
+//                 // No more non-val elements to swap, rest are val
+//                 break;
+//             }
+//         }
+//     }
+//     return ans;
+// }
 
 int main() {
     vector<int> nums = {0, 1, 2, 2, 3, 0, 4, 2};
