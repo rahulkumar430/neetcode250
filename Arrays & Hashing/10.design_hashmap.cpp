fa07225a3d3c0ecf5c34c2ca927a70821a696ddb
Partha Pratim Sahu <sahurahulkumar5@gmail.com>
2025-11-26 00:00:04 +0530

    array and hashing problem 10th added

---
+++ Arrays & Hashing/10.design_hashmap.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Optimised Approach
+class MyHashMap {
+   private:
+    vector<int> hashMap;
+
+   public:
+    MyHashMap() {
+        hashMap.resize(1000001, -1);  // initialize all to -1
+    }
+
+    void put(int key, int value) {
+        hashMap[key] = value;
+    }
+
+    int get(int key) {
+        return hashMap[key];
+    }
+
+    void remove(int key) {
+        hashMap[key] = -1;
+    }
+};
+
+int main() {
+    MyHashMap* obj = new MyHashMap();  // MyHashMap()
+
+    obj->put(1, 1);               // put(1,1)
+    obj->put(2, 2);               // put(2,2)
+    cout << obj->get(1) << endl;  // get(1)
+    cout << obj->get(3) << endl;  // get(3)
+    obj->put(2, 1);               // put(2,1)
+    cout << obj->get(2) << endl;  // get(2)
+    obj->remove(2);               // remove(2)
+    cout << obj->get(2) << endl;  // get(2)
+
+    delete obj;
+    return 0;
+}
