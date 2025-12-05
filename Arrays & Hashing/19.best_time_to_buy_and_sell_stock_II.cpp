52971e890ceeb1b7346b7b157e2cb899fbaf38c9
Partha Pratim Sahu <sahurahulkumar5@gmail.com>
2025-12-05 22:53:53 +0530

    array and hashing problem 19th added

---
+++ Arrays & Hashing/19.best_time_to_buy_and_sell_stock_II.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {7, 1, 5, 3, 6, 4};
+
+    cout << sol.maxProfit(nums) << endl;
+    return 0;
+}
