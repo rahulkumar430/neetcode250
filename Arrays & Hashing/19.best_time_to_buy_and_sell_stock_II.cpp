5e69cb4a75b6b653497b3c26856913f8d2e7fd78
Partha Pratim Sahu <sahurahulkumar5@gmail.com>
2025-12-06 17:37:25 +0530

    array and hashing problem 19th updated

--- Arrays & Hashing/19.best_time_to_buy_and_sell_stock_II.cpp
+++ Arrays & Hashing/19.best_time_to_buy_and_sell_stock_II.cpp
@@ -3,13 +3,27 @@ using namespace std;
 
 class Solution {
    public:
+    // Optimal Solution is simply: sum all positive differences between consecutive days.
     int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n <= 1) return 0;
+
+        int profit = 0;
+
+        // Add every positive difference between consecutive days
+        for (int i = 1; i < n; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
     }
 };
 
 int main() {
     Solution sol;
-    vector<int> nums = {7, 1, 5, 3, 6, 4};
+    vector<int> nums = {1, 2, 3, 4, 5};
 
     cout << sol.maxProfit(nums) << endl;
     return 0;
