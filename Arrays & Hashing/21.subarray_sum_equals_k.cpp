21d0447af63b77e69b770a76ccf6f7879902f33b
Partha Pratim Sahu <sahurahulkumar5@gmail.com>
2025-12-09 23:43:00 +0530

    array and hashing problem 21st added

---
+++ Arrays & Hashing/21.subarray_sum_equals_k.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int subarraySum(vector<int>& nums, int k) {
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, -1, 1, 2};
+    int k = 2;
+
+    cout << sol.subarraySum(nums, k) << endl;
+
+    return 0;
+}
