ac1a05c4affac8c362b3682b3f1f16621967f2a8
Partha Pratim Sahu <sahurahulkumar5@gmail.com>
2025-12-16 23:47:17 +0530

    two pointers problem 3rd added

---
+++ Two Pointers/3.valid_palindrome_II.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+    }
+};
+int main() {
+    Solution sol;
+    string s = "abbda";
+
+    cout << sol.isPalindrome(s) << endl;
+
+    return 0;
+}
