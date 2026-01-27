20da5d8f6a3c489c8208465b8d46962b3ff9241e
Partha Pratim Sahu <sahurahulkumar5@gmail.com>
2026-01-27 16:17:12 +0530

    stack problem 1st updated

--- Stack/1.baseball_game.cpp
+++ Stack/1.baseball_game.cpp
@@ -3,31 +3,67 @@ using namespace std;
 
 class Solution {
    public:
+    // Time & Space Complexity - O(N)
+    // int calPoints(vector<string>& operations) {
+    //     vector<int> record;  // Stack to store scores
+    //     int sum = 0;
+
+    //     for (string str : operations) {
+    //         // Add last two scores
+    //         if (str[0] == '+') {
+    //             record.push_back(record.back() + record[record.size() - 2]);
+    //         }
+    //         // Double last score
+    //         else if (str[0] == 'D') {
+    //             record.push_back(record.back() * 2);
+    //         }
+    //         // Remove last score
+    //         else if (str[0] == 'C') {
+    //             record.pop_back();
+
+    //         }
+    //         // Numeric value
+    //         else {
+    //             record.push_back(stoi(str));
+    //         }
+    //     }
+
+    //     // Total score
+    //     for (int x : record) sum += x;
+
+    //     return sum;
+    // }
+
+    // Time & Space Complexity - O(N)
     int calPoints(vector<string>& operations) {
-        vector<int> record;  // Stack to store scores
+        stack<int> st;  // Stack to store valid scores
         int sum = 0;
 
         for (string str : operations) {
-            // Numeric value (supports multi-digit and negative)
-            if ((str[0] >= '0' && str[0] <= '9') || str[0] == '-') {
-                record.push_back(stoi(str));
-            }
-            // Add last two scores
-            else if (str[0] == '+') {
-                record.push_back(record.back() + record[record.size() - 2]);
-            }
-            // Double last score
-            else if (str[0] == 'D') {
-                record.push_back(record.back() * 2);
-            }
-            // Remove last score
-            else if (str[0] == 'C') {
-                record.pop_back();
+            if (str == "+") {
+                // Sum of last two scores
+                int top1 = st.top();
+                st.pop();
+                int top2 = st.top();
+                st.push(top1);  // Restore top
+                st.push(top1 + top2);
+            } else if (str == "D") {
+                // Double last score
+                st.push(st.top() * 2);
+            } else if (str == "C") {
+                // Remove last score
+                st.pop();
+            } else {
+                // Numeric value (multi-digit or negative)
+                st.push(stoi(str));
             }
         }
 
-        // Total score
-        for (int x : record) sum += x;
+        // Sum all scores
+        while (!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
 
         return sum;
     }
