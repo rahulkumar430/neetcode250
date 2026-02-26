fd3ced411533263accc8a4ba0b5250c2b1653e5d
Partha Pratim Sahu <sahurahulkumar5@gmail.com>
2026-02-26 17:34:05 +0530

    backtracking problem 12th added

---
+++ Backtracking/12.letter_combinations_of_a_phone_number.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Backtracking Approach
+// Time Complexity: O(n * 4^n), Space Complexity: O(n * 4^n)
+class Solution {
+   public:
+    vector<string> ans;
+
+    vector<string> dialLetters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    void backtrack(string& digits, int index, string currStr) {
+        // If we formed a combination of same length as digits
+        if (index == digits.length()) {
+            ans.push_back(currStr);
+            return;
+        }
+
+        string dialLetter = dialLetters[digits[index] - '0'];
+
+        for (int i = 0; i < dialLetter.length(); i++) {
+            currStr.push_back(dialLetter[i]);  // Choose
+
+            backtrack(digits, index + 1, currStr);  // Move to next digit
+
+            currStr.pop_back();  // Undo
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+
+        backtrack(digits, 0, "");
+
+        return ans;
+    }
+};
+
+int main() {
+    string digits = "34";
+
+    Solution sol;
+    vector<string> ans = sol.letterCombinations(digits);
+
+    for (string str : ans) {
+        cout << str << " ";
+    }
+
+    return 0;
+}
